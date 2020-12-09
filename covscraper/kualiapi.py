feac0ba39182daed04fc0979b687ceef13e73d82
James Shuttleworth <csx239@coventry.ac.uk>
2020-12-09 18:43:57 +0000

    Added "deep" option to retrieve assessment details, BoS names, etc. Incomplete

--- covscraper/kualiapi.py
+++ covscraper/kualiapi.py
@@ -32,13 +32,37 @@ def get_uid( session, module ):
 def _decode_mid( data ):
     # TODO
     return json.loads(data)
-  
+
+def get_assessment(session, aid):
+    #TODO: process this
+    #Currently the keys are "0" "1" "2" etc. Could easily change to better keys so they descrive the content
+    url = "    https://coventry.kuali.co/api/cm/options/{aid}"
+    response = session.get( url.format(aid=aid), headers={'Accept': 'application/json, text/plain, */*', "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZDBjOThhODM5OTNiMDAxNGRiMjA1ZiIsImlzcyI6Imt1YWxpLmNvIiwiZXhwIjoxNjA4NzI4MjAyLCJpYXQiOjE2MDc1MTg2MDJ9.iBVoMZWcz-o2apRv-OVY6-L757rIB8WnRYMXgqfWvLs"} )
+    #print("Got: "+response.text)
+    return json.loads(response.text)
+
+def get_component_types(session):
+    url="https://coventry.kuali.co/api/cm/options/types/Component%20Type?limit=1200&active=true"
+    response = session.get( url, headers={'Accept': 'application/json, text/plain, */*', "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZDBjOThhODM5OTNiMDAxNGRiMjA1ZiIsImlzcyI6Imt1YWxpLmNvIiwiZXhwIjoxNjA4NzI4MjAyLCJpYXQiOjE2MDc1MTg2MDJ9.iBVoMZWcz-o2apRv-OVY6-L757rIB8WnRYMXgqfWvLs"} )
+    #print("Got: "+response.text)
+    types= json.loads(response.text)
+    dTypes={}
+    for i in types:
+        dTypes[i["id"]]=i
+    return dTypes
 def get_module_mid( session, module ):
     url = "https://coventry.kuali.co/api/cm/courses/changes/{uid}?denormalize=true"
-  
+    
     uid = get_uid( session, module )
     #print(f"Got UID: {uid}")
     response = session.get( url.format(uid=uid) )
 
     return _decode_mid( response.text )
 
+def get_bos(session, bosid):
+    #urgh, this needs work. Can't just paste these tokens in here
+    #TODO: actual tokens
+    url = "https://coventry.kuali.co/api/v1/groups/{bosid}/"
+    response = session.get( url.format(bosid=bosid), headers={'Accept': 'application/json, text/plain, */*', "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZDBjOThhODM5OTNiMDAxNGRiMjA1ZiIsImlzcyI6Imt1YWxpLmNvIiwiZXhwIjoxNjA4NzI4MjAyLCJpYXQiOjE2MDc1MTg2MDJ9.iBVoMZWcz-o2apRv-OVY6-L757rIB8WnRYMXgqfWvLs"} )
+    #print("Got: "+response.text)
+    return json.loads(response.text)
--- kualiui.py
+++ kualiui.py
@@ -5,6 +5,8 @@ import datetime
 import sys
 import json,yaml
 from optparse import OptionParser
+from nested_lookup import nested_lookup
+
 
 if __name__ == "__main__":
     
@@ -14,6 +16,7 @@ if __name__ == "__main__":
     parser.add_option("-u","--user", dest="user", help="specify Kuali username", metavar="USER", default=None)
     parser.add_option("-p","--password", dest="password", help="specify Kuali password. Not recommended!", metavar="PASS", default=None)
     parser.add_option("-f","--format", dest="format", help="format for result. Valid options are: json, yaml", metavar="FORMAT", default="json", type="choice", choices=["json","yaml"])
+    parser.add_option("-d","--deep", dest="deep", help="When set, also pull referenced objects",  default=False, action="store_true")
     (options,args)=parser.parse_args()
     params = {"user": options.user, "pass": options.password}
     
@@ -28,18 +31,44 @@ if __name__ == "__main__":
     modules={}
     #Add auth details to session
     session = covscraper.auth.Authenticator(params["user"], params["pass"])
+
+    # bosid="4JMHmYpcl"
+
+    # uid=covscraper.kualiapi.get_module_mid(session, "4061CEM")
+    # covscraper.kualiapi.get_bos(session,bosid)
+    
     for i in args:      
         mid = covscraper.kualiapi.get_module_mid(session,i)
         modules[i]=mid
 
-    if len(args)==1: #remove dict wrapping if only one request
-        modules=modules[args[0]]
+    result={"modules":modules}
 
+    #Now check if "deep" is requested and run through references if it is
+    #TODO: all the rest of these
+    if options.deep:
+        result["assessmentType"]=covscraper.kualiapi.get_component_types(session)
+        result["bos"]={}
+        for k in nested_lookup("boardOfStudy",modules):
+            #print(f"BoS: {k}")
+            if not k in result["bos"]:
+                result["bos"][k]=covscraper.kualiapi.get_bos(session,k)
+        result["assessment"]={}
+
+        for ass in nested_lookup("newAssessmentTable", modules):
+            for a in ass:
+                # print("Assessment: ",end="")
+                # print(a)
+
+                if not a["0"] in result["assessment"]:
+                    result["assessment"][a["0"]]=covscraper.kualiapi.get_assessment(session,a["0"])
+            
+        
+    
     out=""
     if options.format=="json":
-        out=json.dumps(modules)
+        out=json.dumps(result)
     elif options.format=="yaml":
-        out=yaml.dump(modules)
+        out=yaml.dump(result)
     else:
         print(f"Unknown format: {options.format}")
         sys.exit(1)
